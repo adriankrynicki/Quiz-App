c1054d942af2a5323c1949c14a672ddc019251bc
Adrian Krynicki <adrian.krynicki88@gmail.com>
2024-03-13 12:18:03 +0100

    JSON

---
+++ script.js
@@ -0,0 +1,83 @@
+let questions = [
+    {
+        "question": "Was ist die Hauptstadt von Frankreich?",
+        "answer_1": "Berlin",
+        "answer_2": "Madrid",
+        "answer_3": "London",
+        "answer_4": "Paris",
+        "rght_answer": 4
+    },
+    {
+        "question": "Welcher Ozean liegt östlich von Afrika?",
+        "answer_1": "Atlantischer Ozean",
+        "answer_2": "Indischer Ozean",
+        "answer_3": "Pazifischer Ozean",
+        "answer_4": "Arktischer Ozean",
+        "rght_answer": 2
+    },
+    {
+        "question": "Was ist der höchste Berg der Welt?",
+        "answer_1": "Mount Kilimanjaro",
+        "answer_2": "Mount Everest",
+        "answer_3": "K2",
+        "answer_4": "Mont Blanc",
+        "rght_answer": 2
+    },
+    {
+        "question": "Welches Land wird als das Land der aufgehenden Sonne bezeichnet?",
+        "answer_1": "China",
+        "answer_2": "Indien",
+        "answer_3": "Japan",
+        "answer_4": "Südkorea",
+        "rght_answer": 3
+    },
+    {
+        "question": "Was ist die längste Fluss in Europa?",
+        "answer_1": "Die Themse",
+        "answer_2": "Die Donau",
+        "answer_3": "Der Rhein",
+        "answer_4": "Die Wolga",
+        "rght_answer": 4
+    },
+    {
+        "question": "Welche Wüste ist die größte der Welt?",
+        "answer_1": "Sahara-Wüste",
+        "answer_2": "Gobi-Wüste",
+        "answer_3": "Arabische Wüste",
+        "answer_4": "Kalahari-Wüste",
+        "rght_answer": 1
+    },
+    {
+        "question": "Was ist die Hauptstadt von Australien?",
+        "answer_1": "Melbourne",
+        "answer_2": "Sydney",
+        "answer_3": "Canberra",
+        "answer_4": "Perth",
+        "rght_answer": 3
+    },
+    {
+        "question": "Was ist die größte Insel der Welt?",
+        "answer_1": "Borneo",
+        "answer_2": "Grönland",
+        "answer_3": "Java",
+        "answer_4": "Neuguinea",
+        "rght_answer": 2
+    },
+    {
+        "question": "Welches Land hat die meisten Zeitzonen?",
+        "answer_1": "Russland",
+        "answer_2": "USA",
+        "answer_3": "China",
+        "answer_4": "Brasilien",
+        "rght_answer": 1
+    },
+    {
+        "question": "Welcher Kontinent hat die höchste Bevölkerungsdichte?",
+        "answer_1": "Afrika",
+        "answer_2": "Asien",
+        "answer_3": "Europa",
+        "answer_4": "Australien",
+        "rght_answer": 2
+    }
+];
+
