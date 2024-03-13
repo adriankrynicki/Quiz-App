94f53e57935a06f27fc68d9dff6320063a3eece1
Adrian Krynicki <adrian.krynicki88@gmail.com>
2024-03-13 13:09:45 +0100

    renderQuestion()

--- script.js
+++ script.js
@@ -81,3 +81,19 @@ let questions = [
     }
 ];
 
+let currentQuestion = 0;
+ 
+function init() {
+    document.getElementById('questionNr').innerHTML = questions.length;
+
+    renderQuestion()
+}
+
+function renderQuestion() {
+    let question = questions[currentQuestion];
+    document.getElementById('questionText').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
